ea9cc88eb2db4f7e1c1ec9252b87e089a517b28c
Joyce Mai <joycemai.exe@gmail.com>
2022-10-22 04:12:54 -0700

    movement script

---
+++ GameJam2022/Assets/Scripts/Joyce/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private float turnSmoothTime = .1f;
+    float turnSmoothVelocity;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;// + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        if (horizontal != 0 || vertical != 0)
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+            
+        controller.Move(direction * speed * Time.deltaTime);
+    }
+}
