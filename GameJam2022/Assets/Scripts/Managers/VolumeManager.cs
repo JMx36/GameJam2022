2f97804575bc98268759fc2fc3f2ba2f33603b18
Haley Vlahos <horseslovehaley@gmail.com>
2022-10-22 17:41:17 -0700

    Volume Manager
    Set up to be able to control volume in a slider.

---
+++ GameJam2022/Assets/Scripts/Managers/VolumeManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer music;
+    [SerializeField] private AudioMixer sfx;
+
+    public void setMusicVol(float volume)
+    {
+        music.SetFloat("MusicVolume", volume);
+    }
+
+    public void setSFXVol(float volume)
+    {
+        sfx.SetFloat("SFXVolume", volume);
+    }
+}
